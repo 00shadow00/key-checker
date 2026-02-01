98e24c9b8723eebe060cddc4df7cc3ed6947cfc0
00shadow00 <48408201+00shadow00@users.noreply.github.com>
2026-02-02 06:10:26 +0800

    Update check.js

--- api/check.js
+++ api/check.js
@@ -1,17 +1,13 @@
 export default function handler(req, res) {
   const key = req.query.key;
 
-  if (key === "12345") {
-    res.status(200).json({
-      status: "ok",
-      message: "Valid key"
-    });
-  } else {
-    res.status(200).json({
-      status: "invalid",
-      message: "Invalid key"
-    });
-  }
+  const VALID_KEYS = ["12345", "ABCDE", "XYZ789"];
+if (VALID_KEYS.includes(key)) {
+    res.status(200).json({status:"ok", message:"Valid key"});
+} else {
+    res.status(200).json({status:"invalid", message:"Invalid key"});
+}
+
 }
 
 
